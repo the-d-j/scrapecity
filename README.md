c9a20c68b664b3d87f9fe593ed38053827be0344
DonnieU <thedonnieutley@gmail.com>
2019-06-20 12:01:36 -0500

    Running with Express, lowdb, and node-cron

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # WIP
 ## Scraper for IG and Twitter
-### Uses node, axios, cheerio, and ems
+### Uses: node, axios, cheerio, ems, express, lowdb, node-cron
 #### Borrowed from and inspired by Wes Bos
 
 Current state (20 Jun 2019):
-Obtains number of followers for a user from both sites
+ - Obtains number of followers for a user from both sites
+ - Runs Express server
+ - Saves data to db using lowdb (and saves to json file)
+ - Runs every minute 24/7 using cron job via node-cron
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+import express from 'express';
 import { getHTML, getTwitterFollowers, getIGFollowers } from './lib/scraper';
+// import db from './lib/db';
+import './lib/cron';
 
-// console.log(getHTML());
+const app = express();
 
-async function go() {
+// console.log(db);
+
+app.get('/scrape', async (req, res, next) => {
+  console.log('Scraping!!');
+
+  const twUsername = 'wesbos';
+  const igUsername = 'wesbos';
+  const twHTML = `https://twitter.com/${twUsername}`;
+  const igHTML = `https://instagram.com/${igUsername}/?__a=1`;
+
   // console.log(await getHTML('https://twitter.com/wesbos/'));
   // getTwitterFollowers(await getHTML('https://twitter.com/wesbos/'));
-  const html = await getHTML('https://twitter.com/wesbos');
+  const html = await getHTML(twHTML);
   const twCount = await getTwitterFollowers(html);
-  // console.log(twCount); // returns a Promise b/c it's a sync fn
+  // console.log(twCount); // returns a Promise b/c it's async fn
   // console.log(await twCount);
-  console.log(`You have ${twCount} Twitter followers`);
+  const igCount = await getIGFollowers(igHTML);
+  // console.log(`You have ${twCount} Twitter followers and ${igCount} Instagram followers`);
+      
+  res.json({igCount, twCount});  
+});
 
-  const igCount = await getIGFollowers('wesbos');
-  console.log(`You have ${igCount} Instagram followers`);
-}
-
-go();
+const listener = app.listen(2093, deets => { 
+  console.log(`Example app running on port ${listener.address().port}`)
+});
